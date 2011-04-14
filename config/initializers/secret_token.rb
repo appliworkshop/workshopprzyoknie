884acbb724db5b907455d17804093a86188af080
Mariusz <mariuszworkshop@gmail.com>
2011-04-14 16:29:46 +0200

    pierwszy commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Workshop::Application.config.secret_token = 'a94f589ed084aafd4ab34eb197c93ee126096a8234a12ece657c1cac45cf3a87fe929f6ed20d1adea658308a9251a36ecb20b27cd3783b7d1c9c2ba43bfd69ff'
